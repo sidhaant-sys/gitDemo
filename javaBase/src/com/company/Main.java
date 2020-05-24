9b0a8ef68da10cf0be5f5282f5ee6841e2580656
sidhaant <sidhaantsakxena007@gmail.com>
2020-05-24 10:12:14 +0530

    Welcome to DogWorld!

--- javaBase/src/com/company/Main.java
+++ javaBase/src/com/company/Main.java
@@ -5,7 +5,13 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
     System.out.println("Jumanjii");
-	
-        
+    Dog dg  = new Dog();
+
+     	String Name ;
+        String Voice ;
+        dg.set__Name__("Whiskey");
+        dg.set__Voice__("Woof!");
+    
+    System.out.println(dg.get__Name__()+" does "+ dg.get__Voice__());
     }
 }
