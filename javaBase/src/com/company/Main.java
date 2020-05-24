ebb9e309e66b8c7ad3d17d1700041563f52a86e2
sidhaant <sidhaantsakxena007@gmail.com>
2020-05-24 10:18:00 +0530

    Welcome to the Cat World!

--- javaBase/src/com/company/Main.java
+++ javaBase/src/com/company/Main.java
@@ -6,12 +6,16 @@ public class Main {
 	// write your code here
     System.out.println("Jumanjii");
     Dog dg  = new Dog();
-
+    Cat ct  = new Cat();
      	String Name ;
         String Voice ;
         dg.set__Name__("Whiskey");
         dg.set__Voice__("Woof!");
+	ct.set__Name__("Rum");
+        ct.set__Voice__("Meow!");
+
     
     System.out.println(dg.get__Name__()+" does "+ dg.get__Voice__());
+    System.out.println(ct.get__Name__()+" does "+ ct.get__Voice__());
     }
 }
