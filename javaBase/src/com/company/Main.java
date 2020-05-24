e4dfb83e0c3b809d8d30293bd9589e6faaa43c0a
sidhaant <sidhaantsakxena007@gmail.com>
2020-05-24 10:24:15 +0530

    Welcome tot the CatWorld!

--- javaBase/src/com/company/Main.java
+++ javaBase/src/com/company/Main.java
@@ -5,7 +5,13 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
     System.out.println("Jumanjii");
-	
-        
+    Cow cw  = new Cow();
+    String Name ;
+    String Voice ;
+    
+    ct.set__Name__("Rum");
+    ct.set__Voice__("Meow!");
+
+    System.out.println(cw.get__Name__()+" does "+ cw.get__Voice__());
     }
 }
